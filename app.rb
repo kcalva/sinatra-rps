c6e5c8327cf2f206eb03c67c012ff684dc32aa6f
Kevin Calva <62715460+kcalva@users.noreply.github.com>
2024-02-21 00:31:15 +0000

    created routes for rock paper scissors game

--- app.rb
+++ app.rb
@@ -2,8 +2,53 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb(:homepage)
 end
+
+get("/rock"){
+
+  moves = ["rock","paper","scissors"]
+  @comp_moves = moves.sample
+
+  if @comp_moves == "rock"
+    @outcome = "tied"
+  elsif @comp_moves == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:zebra)
+}
+
+get("/paper"){
+
+  moves = ["rock","paper","scissors"]
+  @comp_moves = moves.sample
+
+  if @comp_moves == "paper"
+    @outcome = "tied"
+  elsif @comp_moves == "scissors"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:lion)
+}
+
+get("/scissors"){
+
+  moves = ["rock","paper","scissors"]
+  @comp_moves = moves.sample
+
+  if @comp_moves == "scissors"
+    @outcome = "tied"
+  elsif @comp_moves == "rock"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:goat)
+}
